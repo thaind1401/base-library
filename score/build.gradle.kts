03bbfe6a9d33b7e42a72c6c702ebd6d7886d1cb9
mobile.team86 <mobile.team86@gmail.com>
2024-05-10 11:27:44 +0700

    commit

--- score/build.gradle.kts
+++ score/build.gradle.kts
@@ -39,7 +39,7 @@ afterEvaluate {
                 from(components.findByName("release"))
                 groupId = "com.thaind.score"
                 artifactId = "score-calculator"
-                version = "1.0.1"
+                version = "1.0.2"
             }
         }
     }
